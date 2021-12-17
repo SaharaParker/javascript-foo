6ea6de30fd5f2048e14e1ce3a7f86e5f1221da47
Sahara <THsahara@gmail.com>
2021-12-17 18:10:56 -0500

    Put Examples back

--- app.js
+++ app.js
@@ -12,13 +12,13 @@ calcInflate(0.5,0.75)
 
 calcInflate(0.8,0.88)
 
-calcInflate(((0.07)/52) * 100)
 
 function calcInflateUpdated(price_a,price_b, productName = "Milk") {
     const inflationPercent = (price_b - price_a)/price_a * 100;
-    const inflationPercent2 = Math.round(inflationPercent,2);
+    const inflationPercent2 = Math.round(inflationPercent);
     console.log("Inflation Rate of " + productName + " is " + inflationPercent2 + "%")
 
     }
 
     calcInflateUpdated(price_a,price_b,"Milk")
+
