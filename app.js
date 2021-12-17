00154ab3d4291fec4e04eecbfaa7e30064350e66
Sahara <THsahara@gmail.com>
2021-12-17 16:31:34 -0500

    Put Examples back

--- app.js
+++ app.js
@@ -8,6 +8,11 @@ function calcInflate(price_a,price_b){
 
 }
 
+calcInflate(0.5,0.75)
+
+calcInflate(0.8,0.88)
+
+calcInflate(((0.07)/52) * 100)
 
 function calcInflateUpdated(price_a,price_b, productName = "Milk") {
     const inflationPercent = (price_b - price_a)/price_a * 100;
