f370dc930e98a1ffe2e47e1995262835b8cbd915
Sahara <THsahara@gmail.com>
2021-12-17 16:27:50 -0500

    Removed Examples

--- app.js
+++ app.js
@@ -5,19 +5,15 @@ console.log((price_b - price_a)/price_a * 100)
 function calcInflate(price_a,price_b){
     console.log((price_b - price_a)/price_a * 100)
 
+
 }
 
-calcInflate(0.5,0.75)
-
-calcInflate(0.8,0.88)
 
 function calcInflateUpdated(price_a,price_b, productName = "Milk") {
     const inflationPercent = (price_b - price_a)/price_a * 100;
     const inflationPercent2 = Math.round(inflationPercent,2);
-    console.log("Inflation Rate of" + productName + "is" + inflationPercent + "%")
+    console.log("Inflation Rate of " + productName + " is " + inflationPercent2 + "%")
 
     }
 
-
-    }
-}
+    calcInflateUpdated(price_a,price_b,"Milk")
