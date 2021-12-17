da6579161d147fcdb569ce9291d3e484bb44c52a
Sahara <THsahara@gmail.com>
2021-12-17 16:02:32 -0500

    Tried to add and test inflation percent

--- app.js
+++ app.js
@@ -11,10 +11,10 @@ calcInflate(0.5,0.75)
 
 calcInflate(0.8,0.88)
 
-function calcInflate(price_a,price_b, productName = "Milk") {
-    const inflationPercent = (price_b - price_a)/price_a * 100);
+function calcInflateUpdated(price_a,price_b, productName = "Milk") {
+    const inflationPercent = (price_b - price_a)/price_a * 100;
     const inflationPercent2 = Math.round(inflationPercent,2);
-    console.log("Inflation Rate of")
+    console.log("Inflation Rate of" + productName + "is" + inflationPercent + "%")
 
     }
 
